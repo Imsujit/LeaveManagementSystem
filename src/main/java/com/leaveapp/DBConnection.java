6c19b6142faf9b381d2a4bdad34fc1061e14be37
Imsujit <sujitsatpute79@gmail.com>
2026-02-26 01:55:25 +0530

    war file updated

--- src/main/java/com/leaveapp/DBConnection.java
+++ src/main/java/com/leaveapp/DBConnection.java
@@ -5,13 +5,17 @@ import java.sql.DriverManager;
 
 public class DBConnection {
 
-	static String JDBC_HOST = System.getenv("JDBC_HOST") != null ? System.getenv("JDBC_HOST") : "localhost";
-	static String JDBC_PORT = System.getenv("JDBC_PORT") != null ? System.getenv("JDBC_PORT") : "5432";
-	static String JDBC_DB = System.getenv("JDBC_DB") != null ? System.getenv("JDBC_DB") : "leave_management";
-	static String JDBC_USERNAME = System.getenv("JDBC_USERNAME") != null ? System.getenv("JDBC_USERNAME") : "postgres";
-	static String JDBC_PASSWORD = System.getenv("JDBC_PASSWORD") != null ? System.getenv("JDBC_PASSWORD") : "password";
+	private static final String JDBC_HOST = System.getenv("JDBC_HOST") != null ? System.getenv("JDBC_HOST")
+			: "localhost";
+	private static final String JDBC_PORT = System.getenv("JDBC_PORT") != null ? System.getenv("JDBC_PORT") : "5432";
+	private static final String JDBC_DB = System.getenv("JDBC_DB") != null ? System.getenv("JDBC_DB")
+			: "leave_management";
+	private static final String JDBC_USERNAME = System.getenv("JDBC_USERNAME") != null ? System.getenv("JDBC_USERNAME")
+			: "postgres";
+	private static final String JDBC_PASSWORD = System.getenv("JDBC_PASSWORD") != null ? System.getenv("JDBC_PASSWORD")
+			: "password";
 
-	static String JDBC_URL = "jdbc:postgresql://" + JDBC_HOST + ":" + JDBC_PORT + "/" + JDBC_DB;
+	private static final String JDBC_URL = "jdbc:postgresql://" + JDBC_HOST + ":" + JDBC_PORT + "/" + JDBC_DB;
 
 	public static Connection getConnection() {
 		Connection conn = null;
